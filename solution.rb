ac3ac749952070aa725777776b8119b84daf1af7
CristianRojasBetancur <66922367+CristianRojasBetancur@users.noreply.github.com>
2022-08-23 17:06:05 -0500

    la abuela sorda v6

--- solution.rb
+++ solution.rb
@@ -1,15 +1,23 @@
 require 'sinatra'
-require 'sinatra/reloader' if development?
+# require 'sinatra/reloader' if development?
 
 get '/' do
-	erb :index
+  erb :index
 end
 
-post '/abuela' do
-	if params[:mensaje] == params[:mensaje].upcase
-		"<h1>Ahhh si, manzanas!</h1>"
-	else
-		"<h1>Habla más duro mijito</h1>"
-	end
+post '/enviar-mensaje' do
+
+  if params[:mensaje] == params[:mensaje].upcase
+    @mensaje = "Ahhh si, manzanas!"
+  else
+    @mensaje = "Habla más duro mijito"
+  end
+
+  redirect "/abuela/?mensaje=#{@mensaje}"
+end
+
+get '/abuela/' do
+  @mensaje = params[:mensaje]
+  erb :mensaje
 end
 
