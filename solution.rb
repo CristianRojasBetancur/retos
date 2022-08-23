6a9aca358f84bc596dc6232b3e35f40e9c2cf9b5
CristianRojasBetancur <66922367+CristianRojasBetancur@users.noreply.github.com>
2022-08-23 18:05:18 -0500

    Cual es mi navegador

--- solution.rb
+++ solution.rb
@@ -3,12 +3,7 @@ require 'sinatra'
 count = 0
 
 get '/' do
-  @count = count
+  @header = request.env["HTTP_SEC_CH_UA"]
+  
   erb :index
-end
-
-post '/' do
-  puts count
-  count += 1
-  redirect '/'
 end
