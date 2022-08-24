ee4109fe596d1ecd11fb3ae3d7e42fabf868e2f5
CristianRojasBetancur <66922367+CristianRojasBetancur@users.noreply.github.com>
2022-08-24 14:28:47 -0500

    acceso denegado v5

--- solution.rb
+++ solution.rb
@@ -2,5 +2,5 @@ require 'sinatra'
 # require 'sinatra/reloader' if development?
 
 get '/' do
-	request.env['HTTP_PERMISO'] ? "Si lo logramos!" : "Sin permiso"
+	request.env['HTTP_PERMISO'].eql?('soy-un-token-secreto') ? "Si lo logramos!" : "Sin permiso"
 end
